5e8bac6c1417fe950fa2aafe52fa012da4fe4d08
philosolog <philosolog@gmail.com>
2022-07-21 15:32:14 -0700

    edit

--- main.lua
+++ main.lua
@@ -1,3 +1,5 @@
 if game.PlaceId == 1537690962 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/philosolog/sleepy/main/games/Bee-Swarm-Simulator/main.lua"))()
 end
+
+-- TODO: Kill GUI script.
