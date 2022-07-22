f0f3c36166a5cf22eba3f6728cd8483d4484d443
philosolog <philosolog@gmail.com>
2022-07-21 20:37:11 -0700

    Co-authored-by: definedM <definedM@users.noreply.github.com>

--- API/sleepy.lua
+++ API/sleepy.lua
@@ -52,7 +52,7 @@ local sleepy = {
             print(i,v)
         end
     end,
-    ['nickname'] = game.Players.LocalPlayer.Name,
+    ['nickname'] = "pro",
     ['ver'] = ver,
     ['placeid'] = game.PlaceId,
     ['placeversion'] = game.PlaceVersion,
