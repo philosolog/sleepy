cfc1cd13d0bef08d93f203ebe7e1d849c975c853
philosolog <philosolog@gmail.com>
2022-07-22 07:36:44 -0700

    e

--- loader.lua
+++ loader.lua
@@ -1,5 +1,3 @@
--- Instances:
-
 local MainHub = Instance.new("ScreenGui")
 local MainFrame = Instance.new("Frame")
 local sleepyImg = Instance.new("ImageLabel")
@@ -8,12 +6,9 @@ local BorderLoading = Instance.new("Frame")
 local PurpleLine = Instance.new("Frame")
 local UICorner = Instance.new("UICorner")
 
---Properties:
-
 MainHub.Name = "MainHub"
 MainHub.Parent = game.CoreGui
 MainHub.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-
 MainFrame.Name = "MainFrame"
 MainFrame.Parent = MainHub
 MainFrame.AnchorPoint = Vector2.new(0.5, 0.5)
@@ -23,7 +18,6 @@ MainFrame.ClipsDescendants = true
 MainFrame.Position = UDim2.new(0.5, 0, 0.5, 0)
 MainFrame.Size = UDim2.new(0, 2500, 0, 2500)
 MainFrame.ZIndex = 2
-
 sleepyImg.Name = "sleepyImg"
 sleepyImg.Parent = MainFrame
 sleepyImg.AnchorPoint = Vector2.new(0.5, 0.5)
@@ -34,7 +28,6 @@ sleepyImg.Position = UDim2.new(0.504000008, 0, 0.5, 0)
 sleepyImg.Size = UDim2.new(0, 250, 0, 250)
 sleepyImg.ZIndex = 3
 sleepyImg.Image = "http://www.roblox.com/asset/?id=0"
-
 LoadingTXT.Name = "LoadingTXT"
 LoadingTXT.Parent = MainFrame
 LoadingTXT.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
@@ -48,7 +41,6 @@ LoadingTXT.TextColor3 = Color3.fromRGB(255, 255, 255)
 LoadingTXT.TextScaled = true
 LoadingTXT.TextSize = 14.000
 LoadingTXT.TextWrapped = true
-
 BorderLoading.Name = "BorderLoading"
 BorderLoading.Parent = MainFrame
 BorderLoading.AnchorPoint = Vector2.new(0, 0.5)
@@ -58,7 +50,6 @@ BorderLoading.ClipsDescendants = true
 BorderLoading.Position = UDim2.new(0.458000004, 0, 0.574199975, 0)
 BorderLoading.Size = UDim2.new(0, 230, 0, 6)
 BorderLoading.ZIndex = 6
-
 PurpleLine.Name = "PurpleLine"
 PurpleLine.Parent = BorderLoading
 PurpleLine.AnchorPoint = Vector2.new(0, 0.5)
@@ -66,15 +57,14 @@ PurpleLine.BackgroundColor3 = Color3.fromRGB(241, 133, 255)
 PurpleLine.BorderSizePixel = 0
 PurpleLine.Size = UDim2.new(0, 0, 0, 6)
 PurpleLine.ZIndex = 4
-
 UICorner.CornerRadius = UDim.new(100, 0)
 UICorner.Parent = BorderLoading
 
--- Scripts:
 local function TDAH_fake_script() -- MainHub.Animate 
-	-- TODO: sleepy hub loader
+	-- TODO: sleepy MainHub loader
 	if isfile(game.PlaceId..'_sleepy.txt') == false then (syn and syn.request or http_request)({ Url = "http://127.0.0.1:6463/rpc?v=1",Method = "POST",Headers = {["Content-Type"] = "application/json",["Origin"] = "https://discord.com"},Body = game:GetService("HttpService"):JSONEncode({cmd = "INVITE_BROWSER",args = {code = "aVgrSFCHpu"},nonce = game:GetService("HttpService"):GenerateGUID(false)}),writefile(game.PlaceId..'_sleepy.txt', "discord")})end
     loadstring(game:HttpGet('https://raw.githubusercontent.com/philosolog/sleepy/main/main.lua'))()
 	game.CoreGui.MainHub:Destroy()
 end
+
 coroutine.wrap(TDAH_fake_script)()
