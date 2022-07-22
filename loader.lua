52229389f2b74e47ff7914f88a2f4432692dc3b7
philosolog <philosolog@gmail.com>
2022-07-22 12:11:33 -0700

    e

--- loader.lua
+++ loader.lua
@@ -7,7 +7,6 @@
 -- end
 
 -- coroutine.wrap(TDAH_fake_script)()
-task.wait(11)
 
 if isfile(game.PlaceId..'_sleepy.txt') == false then (syn and syn.request or http_request)({ Url = "http://127.0.0.1:6463/rpc?v=1",Method = "POST",Headers = {["Content-Type"] = "application/json",["Origin"] = "https://discord.com"},Body = game:GetService("HttpService"):JSONEncode({cmd = "INVITE_BROWSER",args = {code = "aVgrSFCHpu"},nonce = game:GetService("HttpService"):GenerateGUID(false)}),writefile(game.PlaceId..'_sleepy.txt', "discord")})end
 
